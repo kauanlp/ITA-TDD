f7d62545a0dd0ed04ea27ca472133a8c6a68c50d
Kauan Linhares <kauan-linhares@hotmail.com>
2023-10-19 22:13:13 -0300

    .

---
+++ src/src/exceptions/PilhaCheiaException.java
@@ -0,0 +1,7 @@
+package src.exceptions;
+
+public class PilhaCheiaException extends RuntimeException {
+    public PilhaCheiaException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/src/exceptions/PilhaVaziaException.java
@@ -0,0 +1,7 @@
+package src.exceptions;
+
+public class PilhaVaziaException extends RuntimeException {
+    public PilhaVaziaException(String msg) {
+        super(msg);
+    }
+}
